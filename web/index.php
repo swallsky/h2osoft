903b5eddadaeb1797a13901cdf436f341a2da1f4
ybluesky <xjz1688@163.com>
2016-02-15 17:28:44 +0800

    configs

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <?php
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/ybluesky/h2o/h2o.php');
-(new H2O\web\Application(include(dirname(__DIR__).'/unit/config/web.php')))->run();
+(new H2O\web\Application(include(dirname(__DIR__).'/configs/web.php')))->run();
 ?>
