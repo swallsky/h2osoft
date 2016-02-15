da180e0be76c96ff3b63a308493ecbd1f6a663ab
ybluesky <xjz1688@163.com>
2016-02-15 16:49:12 +0800

    command bat

--- command.bat
+++ command.bat
@@ -1,14 +1,5 @@
 @echo off
 
-rem -------------------------------------------------------------
-rem  Yii command line bootstrap script for Windows.
-rem
-rem  @author Qiang Xue <qiang.xue@gmail.com>
-rem  @link http://www.yiiframework.com/
-rem  @copyright Copyright (c) 2008 Yii Software LLC
-rem  @license http://www.yiiframework.com/license/
-rem -------------------------------------------------------------
-
 @setlocal
 
 set H2O_PATH=%~dp0
